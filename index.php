c901b78456625672eddf3614a2dff4b0dbca02a7
Jen Vitrano <vitranojen@gmail.com>
2016-07-28 10:51:59 -0400

    Update to widget index text

--- index.php
+++ index.php
@@ -5,21 +5,21 @@
 
     <!-- BEGIN INDEX 3 COLUMNS -->
         <section class="row">
-            <div class="four columns">
+            <div class="four columns indextext">
               <?php dynamic_sidebar('index-one'); ?>
               <div class="centerbutton">
                 <a href="http://jenvitrano.com/jvanhartprovisions/about/"><button type="button" class="redbutton">learn more about us</button></a>
               </div>
             </div>
 
-            <div class="four columns">
+            <div class="four columns indextext">
             <?php dynamic_sidebar('index-two'); ?>
               <div class="centerbutton">
                 <a href="http://jenvitrano.com/jvanhartprovisions/products/"><button type="button" class="redbutton">view our products</button></a>
               </div>
             </div>
 
-            <div class="four columns">
+            <div class="four columns indextext">
               <?php dynamic_sidebar('index-three'); ?>
               <div class="centerbutton">
                 <a href="http://jenvitrano.com/jvanhartprovisions/contact-us/"><button type="button" class="redbutton">contact us today</button></a>
